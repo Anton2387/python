d58b030ebe91530d1c1089d4c2226e3df8f102d3
Anton23 <antonavdonin87@gmail.com>
2023-01-25 21:20:35 +0300

    дан список, выводим отдельно буквы и цифры

---
+++ seminar/seminar6/example02.py
@@ -0,0 +1,34 @@
+# 2. Дан список, вывести отдельно буквы и цифры.
+
+a = ( "a", 'b', '2', '3' ,'c')
+# b = ( 'a' , 'b' , 'c')
+# c = ( '2', '3')
+
+b= filter(str.isalpha, a)
+c= filter(str.isdigit, a)
+
+print(*b)
+print(*c)
+
+print('-------------------------')
+
+a = ( "a", 'b', '2', '3' ,'c')
+b = ( 'a' , 'b' , 'c')
+c = ( '1', '2', '3')
+
+x = a+b+c
+print(x)
+
+number_list = []
+char_list = []
+
+
+for i in x: # обработка исключений
+    try:
+        int(i)
+        number_list.append(i)
+    except:
+        char_list.append(i)
+
+print(number_list)
+print(char_list)
